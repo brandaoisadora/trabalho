38d5f07aea8a4f77cabd5ba0074b76acbb6b9b12
isadora brandao <105304311+brandaoisadora@users.noreply.github.com>
2024-08-01 12:30:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -35,38 +35,38 @@ const perguntas = [
         enunciado: "  Uma das questoẽs apresentadas foi como você lida com a educação sendo avançada junto com a tecnologia dentro das escolas e oque você acha sobre isso? ",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Estão lidando muito bem com o novo ensino e a tecnologia, um modo mais prático de ensinar e aprender.",
+                afirmacao: "a educação está avançando junto com a tecnologia."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: " As pessoas estão se desenteressando pela faculdade de pedagogia, pois eles tem que seguir o cronograma do governo.",
+                afirmacao: "a preocupação com professores e seus empregos."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "No final do trabalho, você teria que achar uma forma de representar o novo ensino junto a tecnologia, seja em forma de desenho, frase,etc.",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "os alunos preferem fazer um desenho por aplicativos que tem no computador.",
+                afirmacao: "Modo prático e direto de mostrar que ate desenhos são feitos por internet hoje em dia."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: " Fazer um desenho a mão e com detalhes.",
+                afirmacao: " expressa mais realidade e simplicidde, feito com amor."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "A escola fez uma atividade de pesquisa com os alunos, perguntando se eles estavam se adaptando com o novo modo de ensino das escolas publicas.",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "defende a nova aplicação feita nas escolas, pois pode abrir mais horizontes aos alunos.",
+                afirmacao: " Hoje em dia dependemos da internet para viver."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "totalmente contra, novas diciplinas que não caeem em vestibulares.",
+                afirmacao: "prejudica os alunos, principalmente 3 ano. "
             }
         ]
     },
